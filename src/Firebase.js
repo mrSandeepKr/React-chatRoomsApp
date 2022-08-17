62f46910cc551e304d7c9e4838063564353750cf
mrSandeepKr <sandeep.kumar.ece16@itbhu.ac.in>
2022-08-17 23:35:50 +0530

    intializing firebase crap
    Update Firebase.js
    
    Update index.html

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyApdYef7kEVhXpnTsQe_FG8DseeQoRpEnw",
   authDomain: "whatsapp-clone-29fdf.firebaseapp.com",
@@ -16,6 +12,12 @@ const firebaseConfig = {
   measurementId: "G-B3BTCN0TH2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+const database = getFirestore(app);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export {auth, provider}
+export default database
